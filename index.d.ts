e5924b71147134021118846d1db9e86b16c8c2a7
Simon Schwitzky <hi@saaymeen.com>
2020-04-17 17:21:49 +0200

    Initial commit

--- index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as Dropdown } from './dropdown';
-export { default as Toggle } from './toggle';
-export { default as Option, OptionContainer } from './option';
--- toggle.d.ts
+++
@@ -1,6 +0,0 @@
-import { FC } from 'react';
-interface Props {
-    className?: string;
-}
-declare const Toggle: FC<Props>;
-export default Toggle;
