d199ab3cd31bcc3f73196c53e9276907496b2054
Simon Schwitzky <hi@saaymeen.com>
2020-04-17 21:31:05 +0200

    Point index file inside build folder

--- rollup.config.js
+++ rollup.config.js
@@ -3,23 +3,23 @@ import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
 
-//import pkg from './package.json';
+import pkg from './package.json';
 
 export default {
 	input: 'src/index.ts',
 	output: [
 		{
-			dir: 'build',
+			file: pkg.main,
 			format: 'cjs',
 			exports: 'named',
 			sourcemap: true,
-		} /*,
+		},
 		{
 			file: pkg.module,
 			format: 'es',
 			exports: 'named',
 			sourcemap: true,
-		},*/,
+		},
 	],
 	plugins: [
 		external(),
